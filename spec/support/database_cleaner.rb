33b1214a816395f7410a8a7f7e3c1a2113250481
Singhal <srishabh@amazon.com>
2014-10-07 10:41:17 +0530

    Fixed rspec config

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,12 @@
 RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  config.use_transactional_fixtures = false
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
+  config.before :each do
     DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.append_after(:each) do
+  config.after do
     DatabaseCleaner.clean
   end
 end
